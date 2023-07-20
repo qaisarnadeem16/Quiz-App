256cfdc87ec91e83f56e2780ecd6b182a94ad892
Qaisar Nadeem <101329010+qaisarnadeem16@users.noreply.github.com>
2023-07-20 19:10:42 +0500

    Update index.js

--- server/index.js
+++ server/index.js
@@ -17,6 +17,15 @@ app.use(
       credentials: true,
     })
 );
+app.use((req, res, next) => {
+    res.set({
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Methods": "*",
+        "Access-Control-Allow-Headers": "'Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token'",
+    });
+
+    next();
+});
   app.use("/", express.static("uploads"));
 app.listen(port , ()=>{
     console.log(`listening on port ${port}`)
