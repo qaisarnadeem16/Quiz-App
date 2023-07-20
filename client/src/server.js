29ae5347577a1d1d3692ec07d38afa3909e059b5
Qaisar Nadeem <101329010+qaisarnadeem16@users.noreply.github.com>
2023-07-20 18:14:53 +0500

    Update server.js

--- client/src/server.js
+++ client/src/server.js
@@ -1,3 +1,3 @@
-export const server ='http://localhost:8000/api/v2'
+export const server ='https://quiz-app-mu-rust.vercel.app/api/v2'
 
-export const backend_url ='http://localhost:8000/'
+export const backend_url ='https://quiz-app-mu-rust.vercel.app/'
